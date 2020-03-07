5154515d4f4396c6c6f8075dee439831d400fdeb
Neboer <rubinposter@gmail.com>
2020-03-07 11:45:23 +0800

    增加监测ip地址的功能

--- resolv.py
+++ resolv.py
@@ -1,8 +1,19 @@
 from dns.resolver import Resolver, Answer
 from threading import Thread
+from re import match
+
+
+# 这里的域名解析，预先带有一个ip检查功能。如果输入的域名就是ip地址，解析会直接返回。
+def checkIfIpAddress(check_str):
+    result = match(
+        r"(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])",
+        check_str)
+    return result is not None
 
 
 def resolve_address(hostname, nameserver):
+    if checkIfIpAddress(hostname):
+        return hostname
     res = Resolver('', False)
     res.nameservers = [nameserver]
     result = res.query(hostname)  # type: Answer
